42ed4606b6b22d5d90fc13969ce9edb498c845d5
RaynRus <aleksey@gorbunov.su>
2025-03-16 18:39:29 +0700

    -Сделал вывод видео файлов

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
 from flask import Flask, request, render_template, redirect, session, make_response
 
 from user import create_user_table, User
+from video import create_video_table, Video
 
 import secrets
 
+
+
 create_user_table()
+create_video_table()
 app = Flask("main")
 app.secret_key = secrets.token_hex(32)
 
 @app.route("/")
 def recomend_page():
-    response = make_response('hello world')  # создание объекта ответа
-    response.set_cookie('cookie_name', 'cookie_value', 3600)
-    return response
+    videos = Video.get_all()
+    username = session.get("username", None)
+    user = None
+    if username:
+        user = User.get_user_by_username(username)
+
+    return render_template("index.html", videos=videos, user=user)
 
 @app.route("/registration", methods=["POST", "GET"])
 def register_page():
@@ -39,9 +47,52 @@ def register_page():
         session["username"] = username
         return redirect('/')
 
+@app.route("/profile", methods=["POST", "GET"])
+def profile_page():
+    username = session.get("username")
+
+    user = User.get_user_by_username(username)
+
+    if not username:
+         return redirect("/login")
+
+    if request.method == "GET":
+        videos = Video.get_by_author(user.id)
+        return render_template("profile.html", user=user, videos=videos)
+    if request.method == "POST":
+        title = request.form.get("title")
+        text = request.form.get("text")
+        file_name = request.files["file_name"]
+
+        file_name.save(f"static/uploads/{file_name.filename}")
+
+        Video.create(title, text, file_name.filename, user.id)
+        return redirect('/profile')
 @app.route("/profile/<int:id>")
-def profile_page(id):
+def unknow_profile_page(id):
     pass
+
+
+@app.route("/login", methods=["POST", "GET"])
+def login_page():
+    user_session = session.get("username")
+    print(user_session)
+
+    if request.method == "GET":
+        return render_template("login.html")
+    if request.method == "POST":
+        username = request.form.get("username").lower()
+        password = request.form.get("password")
+
+        user = User.get_user_by_username(username)
+        if not user:
+            return render_template("login.html", error="Неправльный логин или пароль.")
+
+        if user.password == password:
+            session["username"] = username
+            return redirect('/')
+        else:
+            return render_template("login.html", error="Неправильный логин или пароль.")
 
 @app.route("/subscribes")
 def subscribes_page():
@@ -54,6 +105,7 @@ def music_page():
 
 @app.route("/quit")
 def quit_page():
-    pass
+    del session["username"]
+    return redirect('/login')
 
 app.run(host='0.0.0.0', port=8080, debug=True)
--- video.py
+++ video.py
@@ -1,15 +1,16 @@
 import sqlite3
 import datetime
 
-db_name = "data.db"
+db_name = "nartv.db"
 
 
-def create_post_table():
+def create_video_table():
     SQL = """
         CREATE TABLE IF NOT EXISTS video (
             id INTEGER PRIMARY KEY,
             title TEXT,
             text TEXT,
+            file_name VARCHAR(255),
             at_publish TEXT,
             author_id INTEGER
         )
@@ -19,10 +20,11 @@ def create_post_table():
 
 
 class Video:
-    def __init__(self, id, title, text, at_publish, author_id, username=None):
+    def __init__(self, id, title, text, file_name, at_publish, author_id, username=None):
         self.id = id
         self.title = title
         self.text = text
+        self.file_name = file_name
         self.at_publish = at_publish
         self.author_id = author_id
         self.author_username = username
@@ -48,13 +50,13 @@ class Video:
         return [Video(*row) for row in data]
 
     @staticmethod
-    def create(title, text, author_id):
+    def create(title, text, file_name, author_id):
         SQL = """
-            INSERT INTO viedo(title, mp3, at_publish, author_id)
-            VALUES (?, ?, ?, ?)
+            INSERT INTO video(title, text, file_name, at_publish, author_id)
+            VALUES (?, ?, ?, ?, ?)
         """
         con = sqlite3.connect(db_name)
         con.execute(SQL, [
-            title, text, datetime.datetime.now().strftime("%d.%m.%Y %H:%M"), author_id
+            title, text, file_name, datetime.datetime.now().strftime("%d.%m.%Y %H:%M"), author_id
         ])
         con.commit()
